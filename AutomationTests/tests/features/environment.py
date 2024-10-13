137ce1e5e967cc90de137fea563b03b3209b3081
Amanda Bittencourt <64277742+amandatrololo@users.noreply.github.com>
2024-10-13 09:39:00 -0300

    Update environment.py

--- AutomationTests/tests/features/environment.py
+++ AutomationTests/tests/features/environment.py
@@ -3,9 +3,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 def before_feature(context, feature):
-    """
-    Inicializa o driver do Selenium apenas para a feature 'primeiro_desafio_challenging_dom'.
-    """
     if "primeiro_desafio_challenging_dom" in feature.filename:
         service = ChromeService(ChromeDriverManager().install())
         context.driver = webdriver.Chrome(service=service)
@@ -13,31 +10,17 @@ def before_feature(context, feature):
         print(f"Driver inicializado para a feature: {feature.name}")
 
 def after_feature(context, feature):
-    """
-    Fecha o driver do Selenium apenas após a feature 'primeiro_desafio_challenging_dom'.
-    """
     if "primeiro_desafio_challenging_dom" in feature.filename:
         if context.driver:
             context.driver.quit()
         print(f"Driver finalizado para a feature: {feature.name}")
 
 def before_scenario(context, scenario):
-    """
-    Executado antes de cada cenário.
-    """
     print(f"Executando o cenário: [{scenario.name}]")
 
 def after_scenario(context, scenario):
-    """
-    Executado após cada cenário.
-    """
     print(f"Cenário finalizado: {scenario.name} [{scenario.status}]")
 
-# Removendo o after_all_scenarios, pois ele já não é necessário.
-
 def after_all_feature(context, feature):
-    """
-    Executado após todos os cenários de uma feature.
-    """
     if feature.status == 'Status.passed':
         print(f"Feature {feature.name} concluída com sucesso:  [{feature.status}]")
